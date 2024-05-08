83a02b65bcb64c1df48b132a11e9a8892efaee41
Milan Straka <milan@strakovi.com>
2024-05-08 21:47:57 +0200

    Add three reinforce-based assignments.

---
+++ labs/12/reinforce_baseline.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+import argparse
+
+import gymnasium as gym
+import numpy as np
+import torch
+
+import rl_utils
+
+parser = argparse.ArgumentParser()
+# These arguments will be set appropriately by ReCodEx, even if you change them.
+parser.add_argument("--recodex", default=False, action="store_true", help="Running in ReCodEx")
+parser.add_argument("--render_each", default=0, type=int, help="Render some episodes.")
+parser.add_argument("--seed", default=None, type=int, help="Random seed.")
+parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+# For these and any other arguments you add, ReCodEx will keep your default value.
+parser.add_argument("--batch_size", default=..., type=int, help="Batch size.")
+parser.add_argument("--episodes", default=..., type=int, help="Training episodes.")
+parser.add_argument("--hidden_layer_size", default=..., type=int, help="Size of hidden layer.")
+parser.add_argument("--learning_rate", default=..., type=float, help="Learning rate.")
+
+
+class Agent:
+    # Use GPU if available.
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def __init__(self, env: rl_utils.EvaluationEnv, args: argparse.Namespace) -> None:
+        # TODO: Create a suitable policy model. The predict method assumes
+        # it is stored as `self._model` and that it returns logits.
+        #
+        # Apart from the policy network defined in `reinforce` assignment, you
+        # also need a value network for computing the baseline, returning
+        # a single output with no activation.
+        #
+        # Using Adam optimizer with the given `args.learning_rate` for both models
+        # is a good default.
+        raise NotImplementedError()
+
+    # The `rl_utils.typed_torch_function` automatically converts input arguments
+    # to PyTorch tensors of given type, and converts the result to a NumPy array.
+    @rl_utils.typed_torch_function(device, torch.float32, torch.int64, torch.float32)
+    def train(self, states: torch.Tensor, actions: torch.Tensor, returns: torch.Tensor) -> None:
+        # TODO: Perform training.
+        # You should:
+        # - compute the predicted baseline using the baseline model,
+        # - train the policy model, using `returns` - `predicted_baseline` as
+        #   advantage estimate,
+        # - train the baseline model to predict `returns`.
+        #
+        # Note that predicting returns in 0-500 range is challenging for the network, given
+        # that the default initialization tries to keep variance -- it is helpful for the
+        # network if you predict returns in a smaller range.
+        raise NotImplementedError()
+
+    @rl_utils.typed_torch_function(device, torch.float32)
+    def predict(self, states: torch.Tensor) -> np.ndarray:
+        self._model.eval()
+        with torch.no_grad():
+            logits = self._model(states)
+            policy = torch.nn.functional.softmax(logits, dim=-1)
+            return policy
+
+
+def main(env: rl_utils.EvaluationEnv, args: argparse.Namespace) -> None:
+    # Set random seeds and the number of threads
+    np.random.seed(args.seed)
+    if args.seed is not None:
+        torch.manual_seed(args.seed)
+    torch.set_num_threads(args.threads)
+    torch.set_num_interop_threads(args.threads)
+
+    # Construct the agent
+    agent = Agent(env, args)
+
+    # Training
+    for _ in range(args.episodes // args.batch_size):
+        batch_states, batch_actions, batch_returns = [], [], []
+        for _ in range(args.batch_size):
+            # Perform episode
+            states, actions, rewards = [], [], []
+            state, done = env.reset()[0], False
+            while not done:
+                # TODO(reinforce): Choose `action` according to probabilities
+                # distribution (see `np.random.choice`), which you
+                # can compute using `agent.predict` and current `state`.
+                action = ...
+
+                next_state, reward, terminated, truncated, _ = env.step(action)
+                done = terminated or truncated
+
+                states.append(state)
+                actions.append(action)
+                rewards.append(reward)
+
+                state = next_state
+
+            # TODO(reinforce): Compute returns by summing rewards (with discounting)
+
+            # TODO(reinforce): Add states, actions and returns to the training batch
+
+        # TODO(reinforce): Train using the generated batch.
+
+    # Final evaluation
+    while True:
+        state, done = env.reset(start_evaluation=True)[0], False
+        while not done:
+            # TODO(reinforce): Choose a greedy action
+            action = ...
+            state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+
+if __name__ == "__main__":
+    args = parser.parse_args([] if "__file__" not in globals() else None)
+
+    # Create the environment
+    env = rl_utils.EvaluationEnv(gym.make("CartPole-v1"), args.seed, args.render_each)
+
+    main(globals().pop("env"), globals().pop("args"))
--- lectures/lecture12.md
+++ lectures/lecture12.md
@@ -5,6 +5,9 @@
 #### Video: https://lectures.ms.mff.cuni.cz/video/rec/npfl138/2324/npfl138-2324-12-czech.mp4, CZ Lecture
 #### Video: https://lectures.ms.mff.cuni.cz/video/rec/npfl138/2324/npfl138-2324-12-english.mp4, EN Lecture
 #### Questions: #lecture_12_questions
+#### Lecture assignment: reinforce
+#### Lecture assignment: reinforce_baseline
+#### Lecture assignment: reinforce_pixels
 
 Study material for Reinforcement Learning is the [Reinforcement Learning: An Introduction; second edition
 by Richard S. Sutton and Andrew G. Barto](http://incompleteideas.net/book/the-book-2nd.html)
