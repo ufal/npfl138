bcf3f14ea64cee69703a36b6b82aa64f433f579d
Milan Straka <milan@strakovi.com>
2024-10-07 23:46:42 +0200

    Remove the deprecated Pragma header.

--- slides/server.py
+++ slides/server.py
@@ -15,7 +15,6 @@ class SimpleHTTPServer(http.server.ThreadingHTTPServer):
 
         def end_headers(self):
             self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
-            self.send_header("Pragma", "no-cache")
             self.send_header("Expires", "0")
             super().end_headers()
 
