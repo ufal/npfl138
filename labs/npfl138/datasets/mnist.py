5b93e4417d3ead11aaf208b22dd6aa76385c4515
Milan Straka <milan@strakovi.com>
2025-02-19 20:38:40 +0100

    Convert the MNIST dataset to NCHW format.

--- labs/npfl138/datasets/mnist.py
+++ labs/npfl138/datasets/mnist.py
@@ -26,6 +26,7 @@ class MNIST:
     class Dataset(torch.utils.data.Dataset):
         def __init__(self, data: "MNIST.Elements") -> None:
             self._data = {key: torch.as_tensor(value) for key, value in data.items()}
+            self._data["images"] = self._data["images"].view(-1, MNIST.C, MNIST.H, MNIST.W)
             self._size = len(self._data["images"])
 
         @property
