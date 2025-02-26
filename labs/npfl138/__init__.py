280fa7e43495d166f573e9423fca472af5900e64
Milan Straka <milan@strakovi.com>
2025-02-26 20:47:16 +0100

    Add __version__ field and require_version method.

--- labs/npfl138/__init__.py
+++ labs/npfl138/__init__.py
@@ -14,3 +14,4 @@ from .transformed_dataset import TransformedDataset
 # Utils
 from .initializers_override import global_keras_initializers
 from .startup import startup
+from .version import __version__, require_version
---
+++ labs/npfl138/version.py
@@ -0,0 +1,21 @@
+# This file is part of NPFL138 <http://github.com/ufal/npfl138/>.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+__version__ = "2425.1.1"
+
+
+def require_version(required_version: str) -> None:
+    required = required_version.split(".")
+    assert len(required) <= 3, "Expected at most 3 version components"
+
+    required = list(map(int, required))
+    current = list(map(int, __version__.split(".")))
+
+    assert current[:len(required)] >= required, (
+        f"The npfl138>={required_version} is required, but found only {__version__}.\n"
+        f"Please update the npfl138 package by running either:\n"
+        f"- `VENV_DIR/bin/pip install --upgrade npfl138` when using a venv, or\n"
+        f"- `python3 -m pip install --user --upgrade npfl138` otherwise.")
