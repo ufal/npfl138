547f98db8cc8ba25325466fb662c00df2b0b5623
Milan Straka <milan@strakovi.com>
2026-03-19 17:15:34 +0100

    State that instructions are meant for venv in VENV_DIR.

--- labs/npfl138/version.py
+++ labs/npfl138/version.py
@@ -26,5 +26,5 @@ def require_version(required_version: str) -> None:
     assert current[:len(required)] >= required, (
         f"The npfl138>={required_version} is required, but found only {__version__}.\n"
         f"Please update the npfl138 package by running either:\n"
-        f"- `VENV_DIR/bin/pip install --upgrade npfl138` when using a venv, or\n"
+        f"- `VENV_DIR/bin/pip install --upgrade npfl138` when using a venv in VENV_DIR, or\n"
         f"- `python3 -m pip install --user --upgrade npfl138` otherwise.")
