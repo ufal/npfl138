4c926ec4b07abfa099e68d13c6c1314fe32f8662
Milan Straka <milan@strakovi.com>
2026-04-11 19:56:22 +0200

    Fix the links to the methods in the docstrings.

--- labs/npfl138/datasets/common_voice_cs.py
+++ labs/npfl138/datasets/common_voice_cs.py
@@ -9,8 +9,8 @@ The task is to transcribe a given audio sample into a sentence. The dataset cont
 recordings of people speaking in Czech, with input sound waves passed through
 the usual preprocessing—computing
 [Mel-frequency cepstral coefficients (MFCCs)](https://en.wikipedia.org/wiki/Mel-frequency_cepstrum).
-You can repeat this preprocessing on a given audio using the [load_audio][npfl138.datasets.CommonVoiceCs.load_audio]
-and [extract_mfcc][npfl138.datasets.CommonVoiceCs.mfcc_extract] methods.
+You can repeat this preprocessing on a given audio using the [load_audio][npfl138.datasets.common_voice_cs.CommonVoiceCs.load_audio]
+and [extract_mfcc][npfl138.datasets.common_voice_cs.CommonVoiceCs.mfcc_extract] methods.
 
 The dataset is automatically downloaded if necessary, but note that is has
 200MB, so it might take a while. Furthermore, you can listen to the
