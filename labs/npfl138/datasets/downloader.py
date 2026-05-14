0f0d3ef7fae4f62ac764ace0152d1092309b8d45
Milan Straka <milan@strakovi.com>
2026-05-14 08:05:41 +0200

    Allow downloading additional files in download_url_to_file.

--- labs/npfl138/datasets/downloader.py
+++ labs/npfl138/datasets/downloader.py
@@ -9,12 +9,15 @@ import sys
 import torch
 
 
-def download_url_to_file(url_dir: str, filename: str) -> str:
+def download_url_to_file(url_dir: str, filename: str, *additional_filenames: str) -> str:
     """Download the file from a given URL directory and save it as a given filename.
 
     Parameters:
       url: The URL of a directory where to download the file from.
       filename: The name of the file to save the downloaded content to.
+      additional_filenames: Additional filenames to download when the main
+        file is not present and is being downloaded. Useful for downloading
+        also LICENSE or README files.
 
     Returns:
       The path to the file the content was saved to.
@@ -26,5 +29,9 @@ def download_url_to_file(url_dir: str, filename: str) -> str:
     if not os.path.exists(path):
         print(f"Downloading {filename}...", file=sys.stderr)
         torch.hub.download_url_to_file(url=f"{url_dir}/{filename}", dst=path, progress=True)
+
+        for additional in additional_filenames:
+            additional_path = os.path.join(cache_dir, additional) if cache_dir is not None else additional
+            torch.hub.download_url_to_file(url=f"{url_dir}/{additional}", dst=additional_path)
 
     return path
